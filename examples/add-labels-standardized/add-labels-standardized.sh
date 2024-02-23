712aac812ed87e10b96586be1a61bde5cc72fd47
Sam <109683132+kernelsam@users.noreply.github.com>
2024-02-23 15:05:44 -0800

    change indentation

--- examples/add-labels-standardized/add-labels-standardized.sh
+++ examples/add-labels-standardized/add-labels-standardized.sh
@@ -18,8 +18,8 @@ ACTION_SOURCE_FILE=~/senzing.git/github-util/examples/add-labels-standardized/ad
 # Verify parameters.
 
 if [ -z ${GITHUB_ACCESS_TOKEN+x} ]; then
-	echo "GITHUB_ACCESS_TOKEN is not set."
-	exit
+  echo "GITHUB_ACCESS_TOKEN is not set."
+  exit
 fi
 
 # Make the directory to clone repositories into.
@@ -30,43 +30,43 @@ mkdir -p ${GIT_REPOSITORY_DIR}
 
 REPOSITORIES=$(${GITHUB_UTIL_DIR}/github-util.py print-repository-names)
 for REPOSITORY in "${REPOSITORIES[@]}"; do
-	echo "---- ${REPOSITORY} ------------------------------------------"
+  echo "---- ${REPOSITORY} ------------------------------------------"
 
-	DESTINATION_DIR=${GIT_REPOSITORY_DIR}/${REPOSITORY}/.github/workflows
+  DESTINATION_DIR=${GIT_REPOSITORY_DIR}/${REPOSITORY}/.github/workflows
 
-	# Clone repository.
+  # Clone repository.
 
-	cd ${GIT_REPOSITORY_DIR} || {
-		echo "[ERROR] Failed to change directory"
-		exit 1
-	}
-	git clone "git@github.com:Senzing/${REPOSITORY}.git"
+  cd ${GIT_REPOSITORY_DIR} || {
+    echo "[ERROR] Failed to change directory"
+    exit 1
+  }
+  git clone "git@github.com:Senzing/${REPOSITORY}.git"
 
-	# Make repository directory the current working directory.
+  # Make repository directory the current working directory.
 
-	cd ${GIT_REPOSITORY_DIR}/"${REPOSITORY}" || {
-		echo "[ERROR] Failed to change directory"
-		exit 1
-	}
+  cd ${GIT_REPOSITORY_DIR}/"${REPOSITORY}" || {
+    echo "[ERROR] Failed to change directory"
+    exit 1
+  }
 
-	# Checkout current main/main branch.
+  # Checkout current main/main branch.
 
-	git checkout main
-	git checkout main
-	git pull
+  git checkout main
+  git checkout main
+  git pull
 
-	# Manipulate the files in the repository.
+  # Manipulate the files in the repository.
 
-	mkdir -p "${DESTINATION_DIR}"
-	cp ${ACTION_SOURCE_FILE} "${DESTINATION_DIR}"
+  mkdir -p "${DESTINATION_DIR}"
+  cp ${ACTION_SOURCE_FILE} "${DESTINATION_DIR}"
 
-	# Add and commit all changes to local git repository.
+  # Add and commit all changes to local git repository.
 
-	git add --all
-	git commit -a -m "${GIT_MESSAGE}"
+  git add --all
+  git commit -a -m "${GIT_MESSAGE}"
 
-	# Push changes to GitHub
+  # Push changes to GitHub
 
-	git push
+  git push
 
 done
